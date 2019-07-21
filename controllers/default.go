cf9a8f91801ec4edee83f63ea8c274711e4dac24
Erwin Darg <gmhlovehadoop@163.com>
2019-07-21 19:00:56 +0800

    increase /user

--- controllers/default.go
+++ controllers/default.go
@@ -8,8 +8,16 @@ type MainController struct {
 	beego.Controller
 }
 
+type UserController struct {
+	beego.Controller
+}
+
 func (c *MainController) Get() {
 	c.Data["Website"] = "beego.me"
 	c.Data["Email"] = "astaxie@gmail.com"
 	c.TplName = "index.tpl"
+}
+
+func (c *UserController) Get() {
+	c.Ctx.WriteString("hello world")
 }
